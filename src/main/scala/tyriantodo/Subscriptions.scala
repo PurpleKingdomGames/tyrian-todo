971014a06f3463722952f1273bf6c75821140ef2
Dave Smith <davesmith.00000@gmail.com>
2022-06-03 10:59:04 +0100

    Make key events compatible with perf runner

--- src/main/scala/tyriantodo/Subscriptions.scala
+++ src/main/scala/tyriantodo/Subscriptions.scala
@@ -5,19 +5,27 @@ import org.scalajs.dom.KeyboardEvent
 import org.scalajs.dom.window
 import tyrian.*
 
-object Subscriptions:
+object Subscriptions: 
 
   def subscriptions(model: Model): Sub[IO, Msg] =
     Sub.Batch(
       Sub.fromEvent[IO, KeyboardEvent, Msg]("keyup", window) { e =>
         e.keyCode match
-          case 13 =>
-            // Enter key
-            Some(Msg.SubmitTodo)
-
           case 27 =>
             // Escape key
             Some(Msg.StopEditingAll)
+
+          case _ =>
+            None
+      },
+      Sub.fromEvent[IO, KeyboardEvent, Msg](
+        "keydown",
+        window.document.getElementsByClassName("new-todo").item(0)
+      ) { e =>
+        e.keyCode match
+          case 13 =>
+            // Enter key
+            Some(Msg.SubmitTodo)
 
           case _ =>
             None
